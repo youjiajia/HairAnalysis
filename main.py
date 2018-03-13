9d43235c975e6fa2bc05e963179fc7524fa22e95
JasonYou <jasonyou.info@gmail.com>
2018-03-13 11:14:24 +0800

    for test

--- main.py
+++ main.py
@@ -242,8 +242,9 @@ for n in range(len(org_images)):
             topborder.append((x,y))
         x += detect_step
         y += detect_step
-        value = Hair_region[y][x]
-        if value and (Hair_region[y+1][x] and Hair_region[y+1][x] != value) or (Hair_region[y][x+1] and Hair_region[y][x+1] == value):
+        if Hair_region[y] and Hair_region[y][x]:
+            value = Hair_region[y][x]
+            if value and (Hair_region[y+1][x] and Hair_region[y+1][x] != value) or (Hair_region[y][x+1] and Hair_region[y][x+1] == value):
             bottomborder.append((x,y))
     for (x,y) in topborder:
         value = Hair_region[y][x]

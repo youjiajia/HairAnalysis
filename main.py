b3fd7fa96fd2cec7c2699b28b29aaa62029055f3
JasonYou <jasonyou.info@gmail.com>
2018-03-13 10:12:53 +0800

    for test

--- main.py
+++ main.py
@@ -234,10 +234,8 @@ for n in range(len(org_images)):
     nonhair_thr_relative = int(ceil(float( output_image.max() * nonhair_thr ) / 100))
     Hair_region          = output_image >= hair_thr_relative
     NonHair_region       = output_image <= nonhair_thr_relative
-    print Hair_region
-    print Hair_region[0]
-    print Hair_region.astype(np.int)
-    print Hair_region.astype(np.int)[0]
+    for (x, y) in sliding_window(Hair_region, stepSize = detect_step):
+        print Hair_region[x][y]
     cv2.imwrite(args.output_dir + org_images[n].split('/')[-1][:-4] + "-" + "HairDetection-Hair-region.png", Hair_region.astype(np.int)*255)
     cv2.imwrite(args.output_dir + org_images[n].split('/')[-1][:-4] + "-" + "HairDetection-NonHair-region.png", NonHair_region.astype(np.int)*255)
 

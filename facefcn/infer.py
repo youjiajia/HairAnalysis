9806161ee34111b95ec1cefabdddd8e28d49d81b
JasonYou <jasonyou.info@gmail.com>
2018-03-22 00:58:05 +0800

    hair test

--- facefcn/infer.py
+++ facefcn/infer.py
@@ -8,53 +8,55 @@ tic = time.time()
 caffe.set_mode_cpu()
 
 net = caffe.Net('voc-fcn8s-atonce/deploy.prototxt', './snapshot-face-hair-iter-300000.caffemodel', caffe.TEST)
+def infer(name):
+    im = Image.open('./test/' + name )
+    im = im.resize((250, 250), Image.ANTIALIAS)
+    im = im.resize((512, 512), Image.ANTIALIAS)
 
-name = 'WechatIMG463.jpeg'
-im = Image.open('./' + name )
-im = im.resize((250, 250), Image.ANTIALIAS)
-im = im.resize((512, 512), Image.ANTIALIAS)
+    # Turn grayscale images to 3 channels
+    if (im.size.__len__() == 2):
+        im_gray = im
+        im = Image.new("RGB", im_gray.size)
+        im.paste(im_gray)
 
-# Turn grayscale images to 3 channels
-if (im.size.__len__() == 2):
-    im_gray = im
-    im = Image.new("RGB", im_gray.size)
-    im.paste(im_gray)
+    # switch to BGR and substract mean
+    in_ = np.array(im, dtype=np.float32)
+    in_ = in_[:, :, ::-1]
+    in_ -= np.array((104.00698793, 116.66876762, 122.67891434))
+    in_ = in_.transpose((2, 0, 1))
 
-# switch to BGR and substract mean
-in_ = np.array(im, dtype=np.float32)
-in_ = in_[:, :, ::-1]
-in_ -= np.array((104.00698793, 116.66876762, 122.67891434))
-in_ = in_.transpose((2, 0, 1))
+    # shape for input (data blob is N x C x H x W)
+    net.blobs['data'].reshape(1, *in_.shape)
+    net.blobs['data'].data[...] = in_
 
-# shape for input (data blob is N x C x H x W)
-net.blobs['data'].reshape(1, *in_.shape)
-net.blobs['data'].data[...] = in_
+    # run net and take scores
+    net.forward()
 
-# run net and take scores
-net.forward()
+    # Compute SoftMax HeatMap
+    score_background = net.blobs['score_conv'].data[0][0, :, :]  # Background score
+    score_hair = net.blobs['score_conv'].data[0][1, :, :]  # Hair score
+    score_head = net.blobs['score_conv'].data[0][2, :, :]  # Head score
 
-# Compute SoftMax HeatMap
-score_background = net.blobs['score_conv'].data[0][0, :, :]  # Background score
-score_hair = net.blobs['score_conv'].data[0][1, :, :]  # Hair score
-score_head = net.blobs['score_conv'].data[0][2, :, :]  # Head score
+    score_head = np.exp(score_head)
+    score_hair = np.exp(score_hair)
+    score_background = np.exp(score_background)
 
-score_head = np.exp(score_head)
-score_hair = np.exp(score_hair)
-score_background = np.exp(score_background)
+    hmap_head = score_head / (score_head + score_hair + score_background)
+    hmap_hair = score_hair / (score_head + score_hair + score_background)
+    hmap_background = score_background / (score_head + score_hair + score_background)
 
-hmap_head = score_head / (score_head + score_hair + score_background)
-hmap_hair = score_hair / (score_head + score_hair + score_background)
-hmap_background = score_background / (score_head + score_hair + score_background)
+    out_im = np.array(im, dtype=np.float32)
 
-out_im = np.array(im, dtype=np.float32)
+    out_im[:, :, 0] = hmap_hair * 255
+    out_im[:, :, 1] = hmap_head * 255
+    out_im[:, :, 2] = hmap_background * 255
+    print 'Done'
+    toc = time.time()
 
-out_im[:, :, 0] = hmap_hair * 255
-out_im[:, :, 1] = hmap_head * 255
-out_im[:, :, 2] = hmap_background * 255
+    print "Processing time: ", toc-tic, "seconds"
 
-imsave('./test_' + name , out_im)
+    firstpart,secondpart = name.split('.')
+    imsave('./test/test_' + firstpart + str(toc-tic) + '.' + secondpart , out_im)
 
 
-print 'Done'
-toc = time.time()
-print "Processing time: ", toc-tic, "seconds"
+infer('1.jpg')
